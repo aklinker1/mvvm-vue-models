d2f90b2349b9a1065490ec6d3ece606cc054a0b0
Aaron Klinker <aaronklinker1@gmail.com>
2021-04-10 18:48:08 -0500

    feat: Share and resuse state accross view model isntances

--- src/library/vue-model.ts
+++ src/library/vue-model.ts
@@ -1,25 +1,51 @@
-import { Ref } from "vue";
+import { computed, Ref, shallowRef, UnwrapRef, watch } from "vue";
 
-type WrapRefs<T extends Array<any>> = { [Key in keyof T]: Ref<T[Key]> };
+type Arg = number | string | { toString: () => string };
+
+type WrapRefs<T extends Array<Arg>> = { [Key in keyof T]: Ref<T[Key]> };
 
 const modelKeys = new Set<string>();
 
-interface UseViewModel<A extends any[], S> {
-    (...args: WrapRefs<A>): S;
+interface UseViewModel<A extends Arg[], S> {
+  (...args: WrapRefs<A>): S;
 }
 
-export function defineVueModel<A extends any[], S>(
+export function defineVueModel<A extends Arg[], S>(
   modelKey: string,
   setup: (...args: A) => S
 ): UseViewModel<A, S> {
+  // Warn if model keys are the same
   if (modelKeys.has(modelKey)) {
     console.warn(`Multiple view models defined as \"${modelKey}\"`);
   } else {
     modelKeys.add(modelKey);
   }
 
+  const cachedStates: Record<string, S | undefined> = {};
   return function (...args) {
-    const unwrappedArgs = args.map(arg => arg.value) as A;
-    return setup(...unwrappedArgs);
+    const getUnwrappedArgs = () => args.map((arg) => arg.value) as A;
+    const unwrappedArgs = computed(() => getUnwrappedArgs());
+
+    const getArgsPath = (customArgs: A) => {
+      const argStringValues = customArgs.map((arg) => arg.toString());
+      return [modelKey, ...argStringValues].join(".");
+    };
+    const argsPath = computed(() => getArgsPath(unwrappedArgs.value));
+
+    const getState = (argsPath: string) => {
+      const cached = cachedStates[argsPath];
+      if (cached != null) return cached;
+      return (cachedStates[argsPath] = setup(...unwrappedArgs.value));
+    };
+    const state = getState(argsPath.value);
+
+    watch(argsPath, (newArgsPath, oldArgsPath) => {
+      const newState = getState(newArgsPath);
+      for (const key in newState) {
+        const value = newState[key];
+        state[key] = value;
+      }
+    });
+    return state;
   };
 }
